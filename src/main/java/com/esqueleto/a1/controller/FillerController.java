a4cfe1ad40a717d3e95a927d9d91538536de2b53
ArthurS-T <arthur.martins1611@gmail.com>
2026-05-11 01:18:56 -0300

    Segundo commit, controller e model

---
+++ src/main/java/com/esqueleto/a1/controller/FillerController.java
@@ -0,0 +1,7 @@
+package com.esqueleto.a1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FillerController {
+}
---
+++ src/main/java/com/esqueleto/a1/model/FillerModel.java
@@ -0,0 +1,5 @@
+package com.esqueleto.a1.model;
+@
+public class FillerModel {
+    
+}
